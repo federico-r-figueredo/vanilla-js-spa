3ab80817908c5ef81113bb4e284512bb70e63c87
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-12-20 11:06:04 -0300

    Add Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return `
+        <div class="characters">
+            <article class="character-item">
+                <a href="#/1/">
+                    <img src="image" alt="name" />
+                    <h2>Name</h2>
+                </a>
+            </article>
+        </div>
+    `;
+}
+
+export default Home;
