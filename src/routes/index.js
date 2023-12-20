d5930f6a6e4a6f279a976bd6329fa10808f6b920
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-12-20 11:41:06 -0300

    Add router function

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import Header from '../templates/Header';
+import Home from '../pages/Home';
+import Character from '../pages/Character';
+import NotFound from '../pages/NotFound';
+
+const routes = {
+    '/': Home,
+    '/:id': Character
+};
+
+async function router() {
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+
+    header.innerHTML = await Header();
+}
