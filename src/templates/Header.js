af1c7c7464dfe270b9bb3c80134c6fc7ba293288
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-12-20 11:11:30 -0300

    Add Header template

---
+++ src/templates/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+    return `
+        <div class="header-main">
+            <div class="header-logo">
+                <h1>
+                    <a href="/">
+                        Home
+                    </a>
+                </h1>
+            </div>
+            <div class="header-nav">
+                <a href="#/about/">  
+                    About
+                </a>
+            </div>
+        </div>
+    `;
+}
+
+export default Header;
