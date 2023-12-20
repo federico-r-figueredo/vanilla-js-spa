14f857e1dc2ef25011d5d07a284871545711160f
Federico R. Figueredo <federico.r.figueredo@protonmail.com>
2023-12-20 11:28:10 -0300

    Add NotFound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+function NotFound() {
+    const view = /*html*/ `
+        <div class="error-404">
+            <h2>Error 404 - Not Found</h2>
+        </div>
+    `;
+
+    return view;
+}
+
+export default NotFound;
